15476cb6d0ce86210b468424a64e638530ed05fd
Petr Hučík <petr@getdatakick.com>
2019-11-20 08:05:54 +0100

    version 2.2.4

--- blocksocial.php
+++ blocksocial.php
@@ -39,7 +39,7 @@ class blocksocial extends Module
     {
         $this->name = 'blocksocial';
         $this->tab = 'front_office_features';
-        $this->version = '2.2.3';
+        $this->version = '2.2.4';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
 
